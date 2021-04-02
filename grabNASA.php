314e7e610a41352914aacceadb62fced5b504433
mmcauley19 <micohen19@gmail.com>
2021-04-02 12:34:17 -0800

    mostly ready to go

--- grabNASA.php
+++ grabNASA.php
@@ -1,5 +1,6 @@
 #!/usr/bin/php
 <?php 
+/*McAuley April 2021. Just a simple script to grab NASA snow sport stuff */
 
 function get_data($url){
  $ch = curl_init();
@@ -15,16 +16,44 @@ function get_data($url){
 
 $url="https://geo.nsstc.nasa.gov/SPoRT/outgoing/cbb/alaska_grib/";
 $content = get_data($url);
-preg_match_all('/<tbody>(.*?)<\/tbody>/m',$content,$tbody, PREG_SET_ORDER,0);
+
+preg_match_all('/a href=\"(.*?)\">/',$content,$links,PREG_SET_ORDER,0);
+$cmd = "wget ".$url.$links[5][1];
+exec($cmd);
+//var_dump($links);
+
+exit()
+/*
+preg_match_all('/<tbody>(.*?)<\/tbody>/ms',$content,$tbody, PREG_SET_ORDER,0);
 if (!empty($tbody))
  $tbody=$tbody[0][1];
 
-print($tbody);
+//var_dump($tbody);
+
+
+print(gettype($tbody));
+$urlList = explode('<tr><td class="n"><a href="',$tbody);
+print($urlList[3]);
+
+foreach($urlList as $url){
+ $url = 
+
+$i=0;
+//preg_match_all('<a href=(.*?)>/m',$tbody,$link,PREG_SET_ORDER,0);
+//var_dump($link);
+//foreach($tbody as $link){
+// preg_match_all('<a href=(.*?)>/m',$link,$linkText,PREG_SET_ORDER,0);
+// $i+=1;
+ //print($i."\n");
+//}
+
+
+//var_dump($tbody);
  
 
 ?>
 
-
+*/
 
 
 ?>
